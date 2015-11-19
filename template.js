d308e3c18446a6042f556d03111c253ccd1b362d
Andrey Tkachenko <andrey.tkachenko@tapastreet.com>
2015-11-19 11:23:13 +0300

    Removed generate from compiler

--- template.js
+++ template.js
@@ -3,8 +3,16 @@ template = {
     add: function (name, func) {
         this.tempaltes[name] = func;
     },
-    get: function (name) {
-        return this.tempaltes[name];
+    get: function (name, dir) {
+        if (this.tempaltes[name]) {
+            console.log('including ' + name);
+            return this.tempaltes[name];
+        } else if (dir) {
+            console.log('including ' + (dir + '/' + name.replace(/^(\.\/)+/i, '')));
+            return this.tempaltes[dir + '/' + name.replace(/^(\.\/)+/i, '')];
+        } else {
+            throw Error('Unknow file to include ' + name);
+        }
     }
 };
 
